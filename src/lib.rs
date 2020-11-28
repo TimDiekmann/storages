2984e787a3549368b7e1bf39f525209e6877009d
Tim Diekmann <tim.diekmann@3dvision.de>
2020-11-28 23:59:56 +0100

    Add readme to documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![no_std]
+#![cfg_attr(doc, feature(doc_cfg, external_doc))]
+#![cfg_attr(doc, doc(include = "../README.md"))]
