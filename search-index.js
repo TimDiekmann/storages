7b888de9e68c52669cbe31c6a28e4feaf37638cb
TimDiekmann <21277928+TimDiekmann@users.noreply.github.com>
2020-11-28 23:00:41 +0000

    Deploying to gh-pages from  @ 2984e787a3549368b7e1bf39f525209e6877009d 🚀

--- search-index.js
+++ search-index.js
@@ -1,4 +1,4 @@
 var searchIndex = JSON.parse('{\
-"storages":{"doc":"","i":[],"p":[]}\
+"storages":{"doc":"Test Status Coverage Status Docs master Docs.rs Crates.io…","i":[],"p":[]}\
 }');
 addSearchOptions(searchIndex);initSearch(searchIndex);
